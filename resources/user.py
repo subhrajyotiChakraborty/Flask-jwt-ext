7dcaecc166555de2e708988676d74727a0352ea3
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-14 21:35:33 +0530

    learn jwt extended

---
+++ resources/user.py
@@ -0,0 +1,92 @@
+from flask_restful import Resource, reqparse
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import (create_access_token,
+                                create_refresh_token,
+                                jwt_refresh_token_required,
+                                get_jwt_identity,
+                                jwt_required,
+                                get_raw_jwt
+                                )
+from models.user import UserModel
+from blacklist import BLACKLIST
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('password',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+
+
+class UserRegister(Resource):
+    def post(self):
+        data = _user_parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message": "A user with that username already exists"}, 400
+
+        user = UserModel(data['username'], data['password'])
+        user.save_to_db()
+
+        return {"message": "User created successfully."}, 201
+
+
+class User(Resource):
+
+    def get(self, user_id):
+        user = UserModel.find_by_id(user_id)
+
+        if not user:
+            return {"message": "User not found"}, 404
+
+        return user.json()
+
+    def delete(self, user_id):
+        user = UserModel.find_by_id(user_id)
+
+        if not user:
+            return {"message": "User not found"}, 404
+
+        try:
+            user.delete_from_db()
+            return {"message": "User deleted"}, 200
+        except:
+            return {"message": "Some Error Occurred While deleting The User"}
+
+
+class UserLogin(Resource):
+    def post(self):
+        data = _user_parser.parse_args()
+
+        user = UserModel.find_by_username(data['username'])
+
+        if user and safe_str_cmp(user.password, data['password']):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }, 200
+
+        return {"message": "Invalid Credentials!"}, 401
+
+
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()["jti"]
+        BLACKLIST.add(jti)
+        return {"message": "Successfully logged out!"}, 200
+
+
+class TokenRefrash(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        new_access_token = create_access_token(current_user, fresh=False)
+        return {'access_token': new_access_token}
